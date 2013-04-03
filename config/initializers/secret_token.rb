89775cdc97034b5e250e0bf138320b8189ef3ed6
DavidTamez <david_tamez@hotmail.com>
2013-04-02 18:08:16 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CocoaDesserts::Application.config.secret_token = '56db17b5293d55027d583b08c4444f0a177e62a0cb20b0a6e50b1637eda24aabda5cd593f23752dad75d8af4357baad53f7162a9807c83a903c9a07b84d6b725'
